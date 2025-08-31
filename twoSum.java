82d10d5f3b6d3d490ff62fcda9de24a6d3cd6d82
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2025-08-31 10:47:03 +0530

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -1,35 +1,29 @@
-public class twoSum{
-    public static void twoSum(int[] nums, int target) {
+public class twoSum {
+    public static int[] twoSum(int[] nums, int target) {
         int n = nums.length;
         int lp = 0;
         int rp = n - 1;
-        while(lp != rp){
-            if(nums[lp] + nums[rp] == target){      //step 1:- target value same to (lp+rp)
-                System.out.println(lp+" "+rp);
-            }
-            if(nums[lp] + nums[rp] > target){
-                if(nums[lp] < nums[rp]){
-                    lp++;
-                } else{
-                    rp--;
-                }
-                break;
-            }
-            if (nums[lp] + nums[rp] > target) {     //
-                if(nums[lp] < nums[rp]){
-                    lp++;
-                } else{
-                    rp--;
-                }
-                break;
+
+        while (lp < rp) {
+            int sum = nums[lp] + nums[rp];
+
+            if (sum == target) {   // Found the pair
+                return new int[]{lp, rp};
+            } 
+            else if (sum < target) {
+                lp++;   // increase sum
+            } 
+            else {
+                rp--;   // decrease sum
             }
         }
-        System.out.println(lp+" "+rp);
+        return new int[]{-1, -1};  // not found
     }
 
     public static void main(String[] args) {
-        int arr[] = {2,7,11,15};
-        int target = 9;
-        System.out.println(twoSum(arr, target));
+        int arr[] = {3,2,4};
+        int target = 6;
+        int[] ans = twoSum(arr, target);
+        System.out.println(ans[0] + " " + ans[1]); // Output: 0 1
     }
 }
